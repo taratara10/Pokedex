cec2b9b69f6ff1aa687119dfa79f1dcf82a5d871
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-15 18:38:25 +0900

    typeList check size

--- app/src/main/java/com/kabos/pokedex/repository/PokemonRepository.kt
+++ app/src/main/java/com/kabos/pokedex/repository/PokemonRepository.kt
@@ -14,17 +14,32 @@ class PokemonRepository @Inject constructor(private val pokeApiService: PokeApiS
     suspend fun getPokemonSpeciesById(id: Int): Response<PokemonSpecies> =
         pokeApiService.getPokemonSpeciesById(id)
 
-    fun mergePokemonData(info: PokemonInfo, species: PokemonSpecies): Pokemon =
-            Pokemon(
-                id = species.id,
-                name = species.names[0].name,
-                genera = species.genera[0].genus,
-                weight = info.weight,
-                height = info.height,
-                flavor_text = species.flavor_text_entries[0].flavor_text, //todo 0じゃないので修正
-                sprites = info.sprites.front_default,
-                type_one = info.types[0].type.name,
-                type_two = info.types[1].type.name
-            )
+    fun mergePokemonData(info: PokemonInfo, species: PokemonSpecies): Pokemon {
+        val typeSize = info.types.size
+
+        //typeが1つならtype_twoはnull
+        return if (typeSize == 1) Pokemon(
+            id = species.id,
+            name = species.names[0].name,
+            genera = species.genera[0].genus,
+            weight = info.weight,
+            height = info.height,
+            flavor_text = species.flavor_text_entries[0].flavor_text, //todo 0じゃないので修正
+            sprites = info.sprites.front_default,
+            type_one = info.types[0].type.name,
+            type_two = null
+        )
+        else Pokemon(
+            id = species.id,
+            name = species.names[0].name,
+            genera = species.genera[0].genus,
+            weight = info.weight,
+            height = info.height,
+            flavor_text = species.flavor_text_entries[0].flavor_text, //todo 0じゃないので修正
+            sprites = info.sprites.front_default,
+            type_one = info.types[0].type.name,
+            type_two = info.types[1].type.name
+        )
+    }
 
 }
