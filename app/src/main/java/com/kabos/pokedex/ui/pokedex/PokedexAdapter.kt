2ea5d9368a10f9bfa46ba499e37033b3d81bbeeb
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-19 12:10:01 +0900

    implement pokedexadapter

---
+++ app/src/main/java/com/kabos/pokedex/ui/pokedex/PokedexAdapter.kt
@@ -0,0 +1,48 @@
+package com.kabos.pokedex.ui.pokedex
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.kabos.pokedex.databinding.AdapterPokedexBinding
+import com.kabos.pokedex.databinding.AdapterPokedexBinding.inflate
+import com.kabos.pokedex.model.Pokemon
+
+class PokedexAdapter(private val onClick: (Pokemon) -> Unit)
+    :    androidx.recyclerview.widget.ListAdapter<Pokemon, PokedexAdapter.PokedexViewHolder>(DiffCallback) {
+
+        class PokedexViewHolder(private val binding: AdapterPokedexBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Pokemon, onClick: (Pokemon) -> Unit){
+            binding.apply {
+                pokemonData = item
+                root.setOnClickListener {
+                    onClick(item)
+                }
+                executePendingBindings()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokedexViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = inflate(layoutInflater, parent, false)
+        return PokedexViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PokedexViewHolder, position: Int) {
+        holder.bind(getItem(position), onClick)
+    }
+
+}
+
+private object DiffCallback: DiffUtil.ItemCallback<Pokemon>() {
+    override fun areItemsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/kabos/pokedex/ui/pokedex/PokedexFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/pokedex/PokedexFragment.kt
@@ -1,10 +1,11 @@
 package com.kabos.pokedex.ui.pokedex
 
 import androidx.fragment.app.Fragment
+import com.kabos.pokedex.databinding.FragmentPokedexBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PokedexFragment: Fragment(){
 
-    private lateinit var binding: FragmentPoke
+    private lateinit var binding: FragmentPokedexBinding
 }
