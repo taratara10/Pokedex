f1a9ef02d1f7556bd167a393966fd6314236f1d1
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-21 17:38:04 +0900

    add Region enum Class

---
+++ app/src/main/java/com/kabos/pokedex/model/Region.kt
@@ -0,0 +1,12 @@
+package com.kabos.pokedex.model
+
+enum class Region(val start: Int, end: Int) {
+    Kanto(1,151),//Red,Green
+    Johto(152,251), //2 HGSS
+    Hoenn(252,386), //3 Ruby,Sapphire
+    Sinnoh(387,494),//4 DP
+    Unova(495,649), //5 イッシュBW
+    Kaols(650,721), //6 XY
+    Alola(722,809), //7 SM
+    Galar(810,898) //8 SS
+}
