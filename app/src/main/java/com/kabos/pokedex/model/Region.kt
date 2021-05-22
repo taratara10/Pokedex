517902203a2ce8941c498a0e38a270a3c5b20561
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-22 14:06:10 +0900

    add name in Region class

--- app/src/main/java/com/kabos/pokedex/model/Region.kt
+++ app/src/main/java/com/kabos/pokedex/model/Region.kt
@@ -1,12 +1,12 @@
 package com.kabos.pokedex.model
 
-enum class Region(val start: Int, end: Int) {
-    Kanto(1,151),//Red,Green
-    Johto(152,251), //2 HGSS
-    Hoenn(252,386), //3 Ruby,Sapphire
-    Sinnoh(387,494),//4 DP
-    Unova(495,649), //5 イッシュBW
-    Kaols(650,721), //6 XY
-    Alola(722,809), //7 SM
-    Galar(810,898) //8 SS
+enum class Region(val start: Int, end: Int, name: String) {
+    Kanto(1,151, "カントー"),//Red,Green
+    Johto(152,251, "ジョウト"), //2 HGSS
+    Hoenn(252,386,"ホウエン"), //3 Ruby,Sapphire
+    Sinnoh(387,494,"シンオウ"),//4 DP
+    Unova(495,649, "イッシュ"), //5 イッシュBW
+    Kaols(650,721,"カロス"), //6 XY
+    Alola(722,809,"アローラ"), //7 SM
+    Galar(810,898,"ガラル") //8 SS
 }
