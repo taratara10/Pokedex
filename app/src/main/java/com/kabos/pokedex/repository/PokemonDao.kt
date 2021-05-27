3a1a86b2d90991190463395ae56125fbb2e0c2d6
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-27 22:24:44 +0900

    add fun getPokemonById

--- app/src/main/java/com/kabos/pokedex/repository/PokemonDao.kt
+++ app/src/main/java/com/kabos/pokedex/repository/PokemonDao.kt
@@ -1,4 +1,11 @@
 package com.kabos.pokedex.repository
 
+import androidx.room.Dao
+import androidx.room.Query
+import com.kabos.pokedex.model.Pokemon
+
+@Dao
 interface PokemonDao {
+    @Query("SELECT * FROM pokemon WHERE id = :id")
+    suspend fun getPokemonById(id:Int): Pokemon
 }
