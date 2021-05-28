03e0446c3f568cac199b13193ec13c4e8553bb08
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-28 22:06:40 +0900

    add database

--- app/src/main/java/com/kabos/pokedex/repository/PokemonDatabase.kt
+++ app/src/main/java/com/kabos/pokedex/repository/PokemonDatabase.kt
@@ -1,4 +1,10 @@
 package com.kabos.pokedex.repository
 
-class PokemonDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.kabos.pokedex.model.Pokemon
+
+@Database(entities = [Pokemon::class], version = 1)
+abstract class PokemonDatabase: RoomDatabase() {
+    abstract fun pokemonDao(): PokemonDao
 }
