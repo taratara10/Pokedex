edf101c54208d8a842f0dcc613e856a333df24f9
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-05-30 10:38:23 +0900

    update pokemonDao

--- app/src/main/java/com/kabos/pokedex/repository/PokemonDao.kt
+++ app/src/main/java/com/kabos/pokedex/repository/PokemonDao.kt
@@ -3,9 +3,17 @@ package com.kabos.pokedex.repository
 import androidx.room.Dao
 import androidx.room.Query
 import com.kabos.pokedex.model.Pokemon
+import com.kabos.pokedex.model.PokemonInfo
+import com.kabos.pokedex.model.PokemonSpecies
 
 @Dao
 interface PokemonDao {
     @Query("SELECT * FROM pokemons WHERE id = :id")
     suspend fun getPokemonById(id:Int): Pokemon
+
+    @Query( "SELECT * FROM pokemon_info WHERE id = :id")
+    suspend fun getPokemonInfoById(id: Int) :PokemonInfo
+
+    @Query( "SELECT * FROM pokemon_species WHERE id = :id")
+    suspend fun getPokemonSpeciesById(id: Int) :PokemonSpecies
 }
