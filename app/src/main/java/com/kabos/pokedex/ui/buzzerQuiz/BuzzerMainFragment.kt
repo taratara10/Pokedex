b2321f8731dac7c0cb785b20fc4a9c1b25b8b664
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-01 12:23:50 +0900

    setup main fragmnet

--- app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerMainFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerMainFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
-
+import androidx.fragment.app.activityViewModels
+import com.kabos.pokedex.databinding.FragmentBuzzerMainBinding
+import com.kabos.pokedex.ui.viewModel.BuzzerViewModel
 
 class BuzzerMainFragment: Fragment() {
 
+    private lateinit var binding: FragmentBuzzerMainBinding
+    private val buzzerViewModel: BuzzerViewModel by activityViewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentBuzzerMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
