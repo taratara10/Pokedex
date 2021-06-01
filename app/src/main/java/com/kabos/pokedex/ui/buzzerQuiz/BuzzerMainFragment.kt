2fd0823bb662b323774f0ffde0d3bc041a5b8114
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-01 12:16:48 +0900

    add layout resource of buzzerQuiz

--- app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerMainFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerMainFragment.kt
@@ -1,6 +1,20 @@
 package com.kabos.pokedex.ui.buzzerQuiz
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
+
+
 class BuzzerMainFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
