99d10da2dc980283bd780c71213ea617f9677d27
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-02 10:20:12 +0900

    add cardView

--- app/src/main/java/com/kabos/pokedex/ui/viewModel/BuzzerViewModel.kt
+++ app/src/main/java/com/kabos/pokedex/ui/viewModel/BuzzerViewModel.kt
@@ -1,4 +1,17 @@
 package com.kabos.pokedex.ui.viewModel
 
-class BuzzerViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kabos.pokedex.model.Pokemon
+import com.kabos.pokedex.model.Region
+import com.kabos.pokedex.repository.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BuzzerViewModel @Inject constructor(private val repository: PokemonRepository)
+    : ViewModel(){
+
+    val currentRegion: MutableLiveData<Region> = MutableLiveData(Region.Kanto)
+    val currentPokemon: Pokemon? = null
 }
