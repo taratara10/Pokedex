40db198e34ff40dba13a498c29487d81e19e4822
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-03 22:13:57 +0900

    setup quizFragment

--- app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerQuizFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerQuizFragment.kt
@@ -1,8 +1,38 @@
 package com.kabos.pokedex.ui.buzzerQuiz
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.kabos.pokedex.databinding.FragmentBuzzerQuizBinding
+import com.kabos.pokedex.ui.viewModel.BuzzerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BuzzerQuizFragment: Fragment() {
+    private lateinit var binding: FragmentBuzzerQuizBinding
+    private val buzzerViewModel: BuzzerViewModel by activityViewModels()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBuzzerQuizBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            buzzerVM = buzzerViewModel
+        }
+    }
+
+
+
+
+
+
+
+
 }
