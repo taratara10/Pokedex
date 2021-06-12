0f485ea770a3048d7f721553a5a27ba15dbd6d9e
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-12 09:36:31 +0900

    cal barChart height

--- app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerResultFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/buzzerQuiz/BuzzerResultFragment.kt
@@ -34,12 +34,8 @@ class BuzzerResultFragment: Fragment() {
 
         binding.apply {
             buzzerVM = buzzerViewModel
-
-
-//            //表示用サンプルデータの作成//
-//            val xx = listOf(1f,2f,3f, 4f)
-//            val x = listOf<Float>(1f, 2f,3f, 4f, 5f, 6f )//X軸データ
-//            val y = x.map{it*2}//Y軸データ（X軸の2乗）
+            //calc barChart height
+            barChart.layoutParams.height = 30 + buzzerViewModel.numberOfPlayer * 50
             val xAxisPlayerList = mutableListOf(1f, 2f, 3f, 4f, 5f, 6f, 7f).take(buzzerViewModel.numberOfPlayer + 1)
             inflateHorizontalBarChart(barChart, xAxisPlayerList, buzzerViewModel.playerScoreList)
         }
