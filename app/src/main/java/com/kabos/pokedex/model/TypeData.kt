42f2e1e10a8df9c1873eb946b90de5315342a3a9
taratara10 <64339483+taratara10@users.noreply.github.com>
2021-06-13 17:09:45 +0900

    display type image

--- app/src/main/java/com/kabos/pokedex/model/TypeData.kt
+++ app/src/main/java/com/kabos/pokedex/model/TypeData.kt
@@ -2,23 +2,23 @@ package com.kabos.pokedex.model
 
 import com.kabos.pokedex.R
 
-enum class TypeData(val japanese: String, val image: String) {
-    Bug("むし", R.drawable.type_bug.toString()),
-    Dark("あく", R.drawable.type_dark.toString()),
-    Dragon("ドラゴン", R.drawable.type_dragon.toString()),
-    Electric("でんき", R.drawable.type_electric.toString()),
-    Fairy("フェアリー", R.drawable.type_fairy.toString()),
-    Fighting("かくとう", R.drawable.type_flying.toString()),
-    Fire("ほのお", R.drawable.type_fire.toString()),
-    Flying("ひこう", R.drawable.type_flying.toString()),
-    Ghost("ゴースト", R.drawable.type_ghost.toString()),
-    Grass("くさ", R.drawable.type_grass.toString()),
-    Ground("じめん", R.drawable.type_ground.toString()),
-    Ice("こおり", R.drawable.type_ice.toString()),
-    Normal("ノーマル", R.drawable.type_normal.toString()),
-    Poison("どく", R.drawable.type_poison.toString()),
-    Psychic("エスパー", R.drawable.type_psychic.toString()),
-    Rock("いわ", R.drawable.type_rock.toString()),
-    Steel("はがね", R.drawable.type_steel.toString()),
-    Water("みず", R.drawable.type_water.toString())
+enum class TypeData(val japanese: String, val image:Int) {
+    Bug("むし", R.drawable.type_bug),
+    Dark("あく", R.drawable.type_dark),
+    Dragon("ドラゴン", R.drawable.type_dragon),
+    Electric("でんき", R.drawable.type_electric),
+    Fairy("フェアリー", R.drawable.type_fairy),
+    Fighting("かくとう", R.drawable.type_flying),
+    Fire("ほのお", R.drawable.type_fire),
+    Flying("ひこう", R.drawable.type_flying),
+    Ghost("ゴースト", R.drawable.type_ghost),
+    Grass("くさ", R.drawable.type_grass),
+    Ground("じめん", R.drawable.type_ground),
+    Ice("こおり", R.drawable.type_ice),
+    Normal("ノーマル", R.drawable.type_normal),
+    Poison("どく", R.drawable.type_poison),
+    Psychic("エスパー", R.drawable.type_psychic),
+    Rock("いわ", R.drawable.type_rock),
+    Steel("はがね", R.drawable.type_steel),
+    Water("みず", R.drawable.type_water)
 }
