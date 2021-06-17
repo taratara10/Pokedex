5e70417c278e0f4c62cecdc7ee363f45ca37fe8a
taratara10 <shuhei.knhs@gmail.com>
2021-06-17 20:38:09 +0900

    update viewModel

--- app/src/main/java/com/kabos/pokedex/ui/viewModel/FourChoiceViewModel.kt
+++ app/src/main/java/com/kabos/pokedex/ui/viewModel/FourChoiceViewModel.kt
@@ -1,4 +1,138 @@
 package com.kabos.pokedex.ui.viewModel
 
-class FourChoiceViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kabos.pokedex.R
+import com.kabos.pokedex.model.Pokemon
+import com.kabos.pokedex.model.PokemonInfo
+import com.kabos.pokedex.model.PokemonSpecies
+import com.kabos.pokedex.model.Region
+import com.kabos.pokedex.repository.PokemonRepository
+import com.kabos.pokedex.util.QuestionsRadio
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.*
+import javax.inject.Inject
+
+@HiltViewModel
+class FourChoiceViewModel @Inject constructor(private val repository: PokemonRepository) : ViewModel() {
+
+    var currentPokemon: MutableLiveData<Pokemon> = MutableLiveData()
+    var currentRegion: MutableLiveData<Region> = MutableLiveData(Region.Kanto)
+    var currentProgress = MutableLiveData(1)
+    var numberOfQuestion: Int = 10
+
+    var questionIdList = arrayListOf<Int>()
+    var isAnswered: Boolean = false
+    var goResultFragment = MutableLiveData(false)
+
+    var buttonText = MutableLiveData(R.string.next_btn)
+    var isBtnEnable = MutableLiveData(false)
+
+    var isCollapseCardView = MutableLiveData(false)
+    var isCheckedNumberOfQuestionRadio = MutableLiveData(QuestionsRadio.secound)
+
+    fun updateRegion(region: Region) {
+        currentRegion.postValue(region)
+        currentRegion
+    }
+
+    private fun generateQuestionIdList() {
+        val range = mutableListOf<Int>() //Regionの範囲
+        for (i in currentRegion.value!!.start .. currentRegion.value!!.end) {
+            range.add(i)
+        }
+        range.shuffle()
+        questionIdList = range.take(numberOfQuestion) as ArrayList<Int>
+    }
+
+    /**
+     * btnNext ClickListener
+     */
+
+    fun startQuestion() {
+        //reset value
+        currentProgress.postValue(1)
+
+        generateQuestionIdList()
+        getPokemon(questionIdList.first())
+
+        goResultFragment.postValue(false)
+        buttonText.postValue(R.string.next_btn)
+    }
+
+    fun setupNextQuestion() {
+        //checkboxが空ならreturn
+        if (!isAnswered) return else isAnswered = false
+        if (currentProgress.value != numberOfQuestion) {
+            updateQuestion()
+        } else {
+            navigateResultFragment()
+        }
+    }
+
+    private fun updateQuestion() {
+
+    }
+
+    private fun navigateResultFragment() {
+
+    }
+
+    /**
+     * layout callback
+     */
+    private fun incrementCurrentProgress() {
+        currentProgress.value?.let { i ->
+            if (i < numberOfQuestion) currentProgress.value = i + 1
+        }
+    }
+
+    fun isAnsweredAnyChoices() {
+
+
+    }
+    private fun toggleButtonContent() {
+        if (!isAnswered) isBtnEnable.postValue(false)
+        else isBtnEnable.postValue(true)
+    }
+
+
+    /**
+     * getPokemon
+     */
+    private fun getPokemon(id: Int) = viewModelScope.launch {
+        val pokemon = async {
+            return@async repository.mergePokemonData(
+                getPokemonInfo(id).await() as PokemonInfo,
+                getPokemonSpecies(id).await() as PokemonSpecies
+            )
+        }
+        currentPokemon.postValue(pokemon.await())
+    }
+
+    private suspend fun getPokemonInfo(id: Int): Deferred<Any?> = withContext(Dispatchers.IO) {
+        async {
+            repository.getPokemonInfoById(
+                id = id,
+                onFetchFailed = { t ->
+                    t.stackTrace //todo もっとなんかsnackBarとかで表示させたい
+                })?.data
+        }
+    }
+
+    private suspend fun getPokemonSpecies(id: Int): Deferred<Any?> = withContext(Dispatchers.IO) {
+        async {
+            repository.getPokemonSpeciesById(
+                id = id,
+                onFetchFailed = { t ->
+                    t.stackTrace //todo もっとなんかsnackBarとかで表示させたい
+                })?.data
+        }
+    }
+
 }
+
+
+
+
