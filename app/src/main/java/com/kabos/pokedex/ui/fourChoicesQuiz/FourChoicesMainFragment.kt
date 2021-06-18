3335107f1a2eb23710f6f6edbb091f1ebf287085
taratara10 <shuhei.knhs@gmail.com>
2021-06-19 07:59:34 +0900

    add navigation

--- app/src/main/java/com/kabos/pokedex/ui/fourChoicesQuiz/FourChoicesMainFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/fourChoicesQuiz/FourChoicesMainFragment.kt
@@ -1,6 +1,40 @@
 package com.kabos.pokedex.ui.fourChoicesQuiz
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.kabos.pokedex.databinding.FragmentFourChoicesMainBinding
+import com.kabos.pokedex.ui.viewModel.FourChoiceViewModel
+import com.kabos.pokedex.util.NavigateRegionCallback
 
 class FourChoicesMainFragment: Fragment() {
+
+    private lateinit var binding: FragmentFourChoicesMainBinding
+    private val fourChoicesViewModel: FourChoiceViewModel by activityViewModels()
+    private val navigateRegionCallback = object: NavigateRegionCallback {
+        override fun navigateRegionFragment() {
+            val action = ( = true)
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentFourChoicesMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            fourVM = fourChoicesViewModel
+            callback = navigateRegionCallback
+
+        }
+    }
 }
