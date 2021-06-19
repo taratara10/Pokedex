b0b7a07580454b702aaf5801c2ae24b6ff893168
taratara10 <shuhei.knhs@gmail.com>
2021-06-19 09:53:19 +0900

    add fourChoices quiz UI

--- app/src/main/java/com/kabos/pokedex/ui/fourChoicesQuiz/FourChoicesQuizFragment.kt
+++ app/src/main/java/com/kabos/pokedex/ui/fourChoicesQuiz/FourChoicesQuizFragment.kt
@@ -1,4 +1,12 @@
 package com.kabos.pokedex.ui.fourChoicesQuiz
 
-class FourChoicesQuizFragment {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.kabos.pokedex.databinding.FragmentFourChoicesMainBinding
+import com.kabos.pokedex.ui.viewModel.FourChoiceViewModel
+
+class FourChoicesQuizFragment: Fragment() {
+
+    private lateinit var binding: FragmentFourChoicesMainBinding
+    private val fourChoicesViewModel: FourChoiceViewModel by activityViewModels()
 }
